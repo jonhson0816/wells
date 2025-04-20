61973f721269ba0e7b8b5555b5f6dcf5abcf40ad
owoseni1623 <jonhson0816@gmail.com>
2025-04-20 14:16:41 -0700

    render

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://wellsapi.onrender.com',
         changeOrigin: true,
         secure: false,
         // Make sure path rewriting is correct
